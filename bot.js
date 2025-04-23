7d0ddef2251d3e8612e13621a8c5cb9ddb38ac57
pozstudio <poyraz_.86@hotmail.com>
2025-04-23 04:04:33 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,57 @@
+const mineflayer = require('mineflayer');
+
+let bot = null;
+let afkInterval = null;
+
+function spawnBot(ip, port, version) {
+  if (bot) return;
+
+  bot = mineflayer.createBot({
+    host: ip,
+    port: parseInt(port),
+    username: 'PozBot',
+    version: version
+  });
+
+  bot.once('spawn', () => {
+    bot.chat('Bot Sunucuya Bağlandı');
+    bot.chat('/effect give @s invisibility 99999 1 true');
+
+    let rotate = 0;
+    afkInterval = setInterval(() => {
+      if (!bot || !bot.entity) return;
+
+      rotate += Math.PI / 6;
+      bot.look(rotate, 0, true);
+
+      bot.setControlState('jump', true);
+      setTimeout(() => bot.setControlState('jump', false), 300);
+
+      bot.setControlState('sneak', true);
+    }, 4000);
+  });
+
+  bot.on('end', () => {
+    clearInterval(afkInterval);
+    bot = null;
+  });
+
+  bot.on('error', (err) => {
+    console.log("Bot hatası:", err.message);
+    clearInterval(afkInterval);
+    bot = null;
+  });
+}
+
+function stopBot() {
+  if (bot) {
+    bot.chat('Botun Bağlantısı Kesildi');
+    setTimeout(() => bot.quit(), 1000);
+  }
+}
+
+function isBotRunning() {
+  return bot !== null;
+}
+
+module.exports = { spawnBot, stopBot, isBotRunning };
